56cf1200dfe2237cc70720f6969e1c09102adfe0
Koutaroutarou <kw_extaro24karats@ezweb.ne.jp>
2019-05-27 11:50:29 +0900

    Initial Commit

---
+++ gamenseni/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gamenseni
+//
+//  Created by 渡辺航太郎 on 2019/05/27.
+//  Copyright © 2019 litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
